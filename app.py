8d720ea395b6c0e8227d2c73115c59ff712276b8
Darrrii <125715988+Darrrii@users.noreply.github.com>
2024-12-09 13:06:20 +0700

    7laba app.py

--- app.py
+++ app.py
@@ -1,18 +1,68 @@
-# app.py
-from flask import Flask, jsonify
+import json
+from flask import Flask, request, jsonify, send_from_directory
+from flask_limiter import Limiter
 
+# Инициализация приложения Flask и лимитера
 app = Flask(__name__)
+limiter = Limiter(app)
 
-port = 5001  #порт 1,2,3
+# Хранилище данных
+data = {}
 
-@app.route('/health', methods=['GET'])
-def health():
-    return jsonify({"status": "healthy"}), 200
+# Загрузка данных из файла
+def load_data():
+    global data
+    try:
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = {}
 
-@app.route('/process', methods=['GET'])
-def process():
-    instance_id = f"Instance running on port {port}"
-    return jsonify({"instance_id": instance_id}), 200
+# Сохранение данных в файл
+def save_data():
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+# Загрузка данных при старте приложения
+load_data()
+
+# Маршрут для корневого URL, возвращает HTML-страницу
+@app.route('/')
+def home():
+    return send_from_directory('.', 'index.html')
+
+# Установка общего лимита на 100 запросов в сутки
+@limiter.limit("100 per day")
+@app.route('/set', methods=['POST'])
+def set_value():
+    key = request.json.get('key')
+    value = request.json.get('value')
+    data[key] = value
+    save_data()
+    return jsonify({"message": "Key-Value pair saved."}), 201
+
+@limiter.limit("100 per day")
+@app.route('/get/<key>', methods=['GET'])
+def get_value(key):
+    value = data.get(key)
+    if value is not None:
+        return jsonify({"value": value}), 200
+    return jsonify({"error": "Key not found."}), 404
+
+@limiter.limit("10 per minute")
+@app.route('/delete/<key>', methods=['DELETE'])
+def delete_key(key):
+    if key in data:
+        del data[key]
+        save_data()
+        return jsonify({"message": "Key deleted."}), 200
+    return jsonify({"error": "Key not found."}), 404
+
+@limiter.limit("100 per day")
+@app.route('/exists/<key>', methods=['GET'])
+def exists(key):
+    exists = key in data
+    return jsonify({"exists": exists}), 200
 
 if __name__ == '__main__':
-    app.run(port=port)
+    app.run(debug=True)
